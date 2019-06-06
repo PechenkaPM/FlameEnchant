e0e83d9578f11eff3c6e75d9a609e02ea41e0678
yexeed <yexeed2@gmail.com>
2019-06-06 22:24:30 +0300

    какой долбаёб это придумал, как *кто-то, кто захочет дополнить плагин* будет вообще сюда обращаться если функция, ебать, приватная?!?!?!?

--- FlameEnchant/src/Pechenka/flame/FlameEnchant.php
+++ FlameEnchant/src/Pechenka/flame/FlameEnchant.php
@@ -34,7 +34,7 @@ class FlameEnchant extends PluginBase {
     /**
      * @return FlameEnchant
      */
-    private static function getPlugin() { //ненужная функция, можете убрать ее, ошибок не будет, возможно кто-то захочет допилить зачарования и ему она пригодится.
+    public static function getPlugin() { //ненужная функция, можете убрать ее, ошибок не будет, возможно кто-то захочет допилить зачарования и ему она пригодится.
         return self::$instance;
     }
 }
